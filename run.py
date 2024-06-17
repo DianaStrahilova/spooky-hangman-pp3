29b56697eecbd7b2e1ff7895e6d0d4c88eace17e
Diana Strahilova <diana.strahilova91.ds@gmail.com>
2024-06-17 16:53:32 +0000

    Import necessary modules and files

---
+++ run.py
@@ -0,0 +1,12 @@
+import random
+import sys
+from time import sleep
+from words import words
+from textart import RULES
+from textart import LOGO
+from textart import GAME_OVER
+from textart import WINNER
+from textart import BYE
+from hangman_stages import hangman_stages
+import os
+import string
